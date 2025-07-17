b744f875d3272c3971c4687aff6d995b784859c5
BWZ-CODE <mmige24@gmail.com>
2025-07-17 14:39:35 -0500

    Initial Commit

---
+++ Conversor App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Conversor App
+//
+//  Created by Miguel Angel Bohorquez on 17/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Conversor App/Conversor_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Conversor_AppApp.swift
+//  Conversor App
+//
+//  Created by Miguel Angel Bohorquez on 17/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Conversor_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
